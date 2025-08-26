264580ef27429c29df4d9b76864cb4b1d210fc25
carvalhoclarieli <145785057+carvalhoclarieli@users.noreply.github.com>
2025-08-26 07:52:19 -0300

    26/08

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ const botoes = document.querySelectorAll('.parametro-senha__botao');
 botoes[0].onclik = diminuiTamanho;
 botoes[1].onclik = aumentaTamanho;
 
-function diminuiTamanho(){
-    if(tamanhoSenha > 1){
+function diminuiTamanho() {
+    if (tamanhoSenha > 1) {
         tamanhoSenha = tamanhoSenha - 1;
     }
-    numeroSenha.textContent=tamanhoSenha;
+    numeroSenha.textContent = tamanhoSenha;
 }
